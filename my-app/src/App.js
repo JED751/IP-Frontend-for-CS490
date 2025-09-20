31f157fe12f38b1089a3e21b2823910f469a9734
Mitchell Lipnitsky <jediminor751@gmail.com>
2025-09-19 20:22:47 -0400

    top5 films feature added frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [films, setFilms] = useState([]); //list of films and function to update the films
+
+  useEffect(() => { //function to alter a component
+
+
+    fetch("http://127.0.0.1:5000/api/home") //fetching from flask to get the film data in MySQL since it is connected to it
+      .then(object => object.json()) //object converted to json
+      .then(setFilms) //set films is updated with the values of films
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={{fontFamily: "system-ui", padding: 24}}>
+
+      <h2>Top 5 Rented Films of All Time</h2>
+      <ul>
+        {films.map(f => ( //maps each film
+          <li key={f.film_id}>
+            {f.title} has <b>{f.rentals_count}</b> rentals
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
-export default App;
+export default App; //exporting the app
